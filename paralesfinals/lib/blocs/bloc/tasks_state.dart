5a1536130b90ff2e0f23c7a344be7daa172bba62
Maegan <maegan.parales@usjr.edu.ph>
2022-08-06 02:59:56 +0800

    Wrapped ListTask with widget

--- paralesfinals/lib/blocs/bloc/tasks_state.dart
+++ paralesfinals/lib/blocs/bloc/tasks_state.dart
@@ -5,8 +5,17 @@ part of 'tasks_bloc.dart';
 
 class TasksState extends Equatable {
   final List<Task> allTasks;
+  final List<Task> pendingTasks;
+  final List<Task> completedTasks;
+  final List<Task> favoriteTasks;
+  final List<Task> removedTasks;
+
   const TasksState({
     this.allTasks = const <Task>[],
+    this.pendingTasks = const <Task>[],
+    this.completedTasks = const <Task>[],
+    this.favoriteTasks = const <Task>[],
+    this.removedTasks = const <Task>[],
   });
 
   @override
@@ -15,12 +24,24 @@ class TasksState extends Equatable {
   Map<String, dynamic> toMap() {
     return {
       'allTasks': allTasks.map((x) => x.toMap()).toList(),
+      'pendingTasks': allTasks.map((x) => x.toMap()).toList(),
+      'completedTasks': allTasks.map((x) => x.toMap()).toList(),
+      'favoriteTasks': allTasks.map((x) => x.toMap()).toList(),
+      'removedTasks': allTasks.map((x) => x.toMap()).toList(),
     };
   }
 
   factory TasksState.fromMap(Map<String, dynamic> map) {
     return TasksState(
       allTasks: List<Task>.from(map['allTasks']?.map((x) => Task.fromMap(x))),
+      pendingTasks:
+          List<Task>.from(map['pendingTasks']?.map((x) => Task.fromMap(x))),
+      completedTasks:
+          List<Task>.from(map['completedTasks']?.map((x) => Task.fromMap(x))),
+      favoriteTasks:
+          List<Task>.from(map['favoriteTasks']?.map((x) => Task.fromMap(x))),
+      removedTasks:
+          List<Task>.from(map['allTasks']?.map((x) => Task.fromMap(x))),
     );
   }
 }
--- paralesfinals/lib/widgets/tasks_drawer.dart
+++ paralesfinals/lib/widgets/tasks_drawer.dart
@@ -1,5 +1,8 @@
+import 'package:paralesfinals/blocs/bloc_exports.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../blocs/bloc/tasks_bloc.dart';
 import '../screens/recycle_bin_screen.dart';
 import '../screens/tabs_screen.dart';
 import '../test_data.dart';
@@ -26,15 +29,36 @@ class TasksDrawer extends StatelessWidget {
                 style: Theme.of(context).textTheme.headline5,
               ),
             ),
-            ListTile(
-              leading: const Icon(Icons.folder_special),
-              title: const Text('My Tasks'),
-              trailing: Text(
-                '${TestData.pendingTasks.length} | ${TestData.completedTasks.length}',
+            GestureDetector(
+              child: ListTile(
+                leading: const Icon(Icons.folder_special),
+                title: const Text('My Tasks'),
+                trailing: Text(
+                  '${TestData.pendingTasks.length} | ${TestData.completedTasks.length}',
+                ),
+                onTap: () => Navigator.pushReplacementNamed(
+                  context,
+                  TabsScreen.path,
+                ),
               ),
-              onTap: () => Navigator.pushReplacementNamed(
-                context,
-                TabsScreen.path,
+            ),
+            GestureDetector(
+              child: BlocBuilder<TasksBloc, TasksState>(
+                builder: (context, state) {
+                  return GestureDetector(
+                    child: ListTile(
+                      leading: const Icon(Icons.folder_special),
+                      title: const Text('My Tasks'),
+                      trailing: Text(
+                        '${state.pendingTasks.length} | ${state.completedTasks.length}',
+                      ),
+                      onTap: () => Navigator.pushReplacementNamed(
+                        context,
+                        TabsScreen.path,
+                      ),
+                    ),
+                  );
+                },
               ),
             ),
             const Divider(),
@@ -64,3 +88,155 @@ class TasksDrawer extends StatelessWidget {
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+// import '../blocs/bloc_exports.dart';
+// import '../screens/recycle_bin_screen.dart';
+// import '../screens/tabs_screen.dart';
+// import '../test_data.dart';
+
+// class TasksDrawer extends StatelessWidget {
+//   const TasksDrawer({Key? key}) : super(key: key);
+
+//   _switchToDarkTheme(BuildContext context, bool isDarkTheme) {
+//     if (isDarkTheme) {
+//     } else {}
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SafeArea(
+//       child: Drawer(
+//         child: Column(
+//           children: [
+//             Container(
+//               width: double.infinity,
+//               padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 20),
+//               child: Text(
+//                 'Task Drawer',
+//                 style: Theme.of(context).textTheme.headline5,
+//               ),
+//             ),
+//             BlocBuilder<TasksBloc, TasksState>(
+//               builder: (context, state) {
+//                 return GestureDetector(
+//                   child: ListTile(
+//                     leading: const Icon(Icons.folder_special),
+//                     title: const Text('My Tasks'),
+//                     trailing: Text(
+//                       '${state.pendingTasks.length} | ${state.completedTasks.length}',
+//                     ),
+//                     onTap: () => Navigator.pushReplacementNamed(
+//                       context,
+//                       TabsScreen.path,
+//                     ),
+//                   ),
+//                 );
+//               },
+//             ),
+//             const Divider(),
+//             BlocBuilder<TasksBloc, TasksState>(builder: (context, state) {
+//               return GestureDetector(
+//                 child: ListTile(
+//                   leading: const Icon(Icons.delete),
+//                   title: const Text('Recycle Bin'),
+//                   trailing: Text('${state.removedTasks.length}'),
+//                   onTap: () => Navigator.pushReplacementNamed(
+//                     context,
+//                     RecycleBinScreen.path,
+//                   ),
+//                 ),
+//               );
+//             }),
+//             const Divider(),
+//             const Expanded(child: SizedBox()),
+//             BlocBuilder<SwitchBloc, SwitchState>(builder: (context, state) {
+//               return ListTile(
+//                 leading: Switch(
+//                     value: state.switchValue,
+//                     onChanged: (newValue) {
+//                       newValue
+//                           ? context.read<SwitchBloc>().add(SwitchOnEvent())
+//                           : context.read<SwitchBloc>().add(SwitchOffEvent());
+//                     }),
+//                 title: const Text('Switch to Dark Theme'),
+//                 onTap: () => _switchToDarkTheme(context, !TestData.isDarkTheme),
+//               );
+//             }),
+//             const SizedBox(height: 10),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+////////////////////////////////////////////////////////
+
+// import 'package:flutter/material.dart';
+
+// import '../screens/recycle_bin_screen.dart';
+// import '../screens/tabs_screen.dart';
+// import '../test_data.dart';
+
+// class TasksDrawer extends StatelessWidget {
+//   const TasksDrawer({Key? key}) : super(key: key);
+
+//   _switchToDarkTheme(BuildContext context, bool isDarkTheme) {
+//     if (isDarkTheme) {
+//     } else {}
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SafeArea(
+//       child: Drawer(
+//         child: Column(
+//           children: [
+//             Container(
+//               width: double.infinity,
+//               padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 20),
+//               child: Text(
+//                 'Task Drawer',
+//                 style: Theme.of(context).textTheme.headline5,
+//               ),
+//             ),
+//             ListTile(
+//               leading: const Icon(Icons.folder_special),
+//               title: const Text('My Tasks'),
+//               trailing: Text(
+//                 '${TestData.pendingTasks.length} | ${TestData.completedTasks.length}',
+//               ),
+//               onTap: () => Navigator.pushReplacementNamed(
+//                 context,
+//                 TabsScreen.path,
+//               ),
+//             ),
+//             const Divider(),
+//             ListTile(
+//               leading: const Icon(Icons.delete),
+//               title: const Text('Recycle Bin'),
+//               trailing: Text('${TestData.removedTasks.length}'),
+//               onTap: () => Navigator.pushReplacementNamed(
+//                 context,
+//                 RecycleBinScreen.path,
+//               ),
+//             ),
+//             const Divider(),
+//             const Expanded(child: SizedBox()),
+//             ListTile(
+//               leading: Switch(
+//                 value: TestData.isDarkTheme,
+//                 onChanged: (newValue) => _switchToDarkTheme(context, newValue),
+//               ),
+//               title: const Text('Switch to Dark Theme'),
+//               onTap: () => _switchToDarkTheme(context, !TestData.isDarkTheme),
+//             ),
+//             const SizedBox(height: 10),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
