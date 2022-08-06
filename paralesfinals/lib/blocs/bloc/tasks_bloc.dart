da821e0bc5ac9c0b2284ec81a56b5595fa85d0e1
Maegan <maegan.parales@usjr.edu.ph>
2022-08-06 21:49:59 +0800

    Edited tasks_block

--- paralesfinals/lib/blocs/bloc/tasks_bloc.dart
+++ paralesfinals/lib/blocs/bloc/tasks_bloc.dart
@@ -1,18 +1,15 @@
 import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
-
 import 'package:paralesfinals/models/task.dart';
-
-import '../bloc_exports.dart';
+import 'package:equatable/equatable.dart';
 
 part 'tasks_event.dart';
 part 'tasks_state.dart';
 
-class TasksBloc extends HydratedBloc<TasksEvent, TasksState> {
+class TasksBloc extends Bloc<TasksEvent, TasksState> {
   TasksBloc() : super(const TasksState()) {
     on<AddTask>(_onAddTask);
     on<UpdateTask>(_onUpdateTask);
-    // on<DeleteTask>(_onDeletedTask);
+    on<DeleteTask>(_onDeleteTask);
   }
 
   void _onAddTask(AddTask event, Emitter<TasksState> emit) {
@@ -22,33 +19,7 @@ class TasksBloc extends HydratedBloc<TasksEvent, TasksState> {
     ));
   }
 
-  void _onUpdateTask(UpdateTask event, Emitter<TasksState> emit) {
-    final state = this.state;
-    final task = event.task;
-    final int index = state.allTasks.indexOf(task);
-    List<Task> allTasks = List.from(state.allTasks)..remove(task);
-    task.isDone == false
-        ? allTasks.insert(index, task.copyWith(isDone: true))
-        : allTasks.insert(index, task.copyWith(isDone: false));
-    emit(TasksState(allTasks: allTasks));
-  }
+  void _onUpdateTask(UpdateTask event, Emitter<TasksState> emit) {}
 
-  void _onDeleteTask(AddTask event, Emitter<TasksState> emit) {
-    final state = this.state;
-    emit(
-      TasksState(
-        allTasks: List.from(state.allTasks)..remove(event.task),
-      ),
-    );
-  }
-
-  @override
-  TasksState? fromJson(Map<String, dynamic> json) {
-    return TasksState.fromMap(json);
-  }
-
-  @override
-  Map<String, dynamic>? toJson(TasksState state) {
-    return state.toMap();
-  }
+  void _onDeleteTask(DeleteTask event, Emitter<TasksState> emit) {}
 }
